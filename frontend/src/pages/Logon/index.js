220c08a77c377b4e9fba33207a534b4d2cac86a3
MatheusHG <56003521+MatheusHG@users.noreply.github.com>
2020-04-27 14:18:08 -0300

    Update index.js

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { Link, useHistory } from 'react-router-dom';
 
 import GoogleLogin from 'react-google-login';
 
@@ -7,8 +8,12 @@ import './styles.css';
 
 import logo from '../../assets/logo2.png';
 import banner from '../../assets/banner.jpg';
+import informacao from '../../assets/information.png';
+import atencao from '../../assets/error.png';
 
 export default function Logon(){
+
+    const [modalIsOpen, setModalIsOpen] = useState(false)
     const history = useHistory()
     const responseGoogle = (response) => {
         console.log(response)
@@ -32,11 +37,27 @@ export default function Logon(){
                     onFailure={responseGoogle}
                     cookiePolicy={'single_host_origin'}
                     />
+
+                    <Link className="information" onClick={() => setModalIsOpen(true)}>
+                        <img src={informacao} alt="informação"/>
+                    </Link>
+
+                    <Modal className="janelaModal" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                        <img src={atencao} alt="informação" />
+                        <h1>
+                            Para utilizar o login via Google, deve-se desativar os Cookies de Terceiros e atualizar o site
+                        </h1>
+                        <h2>
+                            Configurações -> Privacidade e Segurança -> Configurações do Site -> Cookies e dados do site -> Desativar o "Bloquear cookies de terceiros"
+                        </h2>
+                        <div>
+                            <button onClick={() => setModalIsOpen(false)}> Close </button>
+                        </div>
+                    </Modal>
                 </form>
-            
             </section>
 
-            <img src={banner} alt="Banner" />
+            <img className="banner" src={banner} alt="Banner" />
         </div>
     );
 }
